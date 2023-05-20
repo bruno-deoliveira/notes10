c012476f57bcaaef1ff6e002ae3a3e1c7417ad36
Bruno <brunoalmeida0805@gmail.com>
2023-05-19 22:42:50 -0300

    adicionado o groupBy

--- src/controllers/NotesController.js
+++ src/controllers/NotesController.js
@@ -5,7 +5,7 @@ class NotesController {
     const { title, description, tags, links } = request.body;
     const user_id = request.user.id;
 
-    const note_id = await knex("notes").insert({
+    const [note_id] = await knex("notes").insert({
       title,
       description,
       user_id
@@ -30,7 +30,7 @@ class NotesController {
 
     await knex("tags").insert(tagsInsert);
 
-    response.json();
+    return response.json();
   }
 
   async show(request, response){
@@ -76,6 +76,7 @@ class NotesController {
         .whereLike("notes.title",`%${title}%`)
         .whereIn("name", filterTags)
         .innerJoin("notes", "notes.id", "tags.note_id")
+        .groupBy("notes.id")
         .orderBy("notes.title")
 
     } else {
